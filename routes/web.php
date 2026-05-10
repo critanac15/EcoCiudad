d799b1a59109842036baf5162aa290376b5a9892
critanac15 <cristiann0815@gmail.com>
2026-05-10 10:57:33 -0500

    Agregando comentarios y rutas para el login y registro

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Rutas para la pagina de inicio
 Route::get('inicio',function(){
     return view('inicio');
 })->name('inicio');
 
+//Rutas para la pagina de reportes
 Route::get('/reportes',function(){
     return view('reportes');
 })->name('reportes');
-
 Route::get('/reportes', [ReportController::class, 'index'])->name('reportes');
 
+//Rutas para soporte y ayuda
 Route::get('soporteAyuda',function(){
     return view('soporteAyuda');
 })->name('soporteAyuda');
 
+//Rutas para login y registro
 Route::get('login',function(){
     return view('login');
 })->name('login');
+Route::get('registro',function(){
+    return view('registro');
+})->name('registro');
