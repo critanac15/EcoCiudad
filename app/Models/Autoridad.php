b83243d71ab1ffe6c96d1c5820d24b68862f1f6a
critanac15 <cristiann0815@gmail.com>
2026-05-31 22:29:53 -0500

    Creando el modelo para la autoridad

---
+++ app/Models/Autoridad.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Autoridad extends Model
+{
+    //Declaracion de variables con protected para que esten protegidas
+    protected $table = 'autoridades';
+    protected $primaryKey = 'id_autoridad';
+
+    //funcion para la relacion en forma eloquentt
+    //has many= tiene muchos, relacion de 1 a  N
+    public function reportes() {
+        return $this->hasMany(Reporte::class, 'id_autoridad');
+    }
+}
