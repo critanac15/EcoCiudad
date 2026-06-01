830ef4bc259acb50da820aff00e5b2b9c06fddb3
critanac15 <cristiann0815@gmail.com>
2026-05-31 22:30:02 -0500

    Creando el modelo para la imagen

---
+++ app/Models/Imagen.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Imagen extends Model
+{
+    //declaracion de una variable asignandole el ID de la tabla
+    protected $primaryKey = 'id_imagen';
+
+    public function reporte() {
+        //hasOne relacon de uno a uno
+        return $this->hasOne(Reporte::class, 'id_imagen');
+    }
+}
