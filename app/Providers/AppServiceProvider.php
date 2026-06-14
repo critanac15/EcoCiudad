3508102237b7c03c15e6344194dece99b3fcce68
critanac15 <cristiann0815@gmail.com>
2026-06-14 13:29:54 -0500

    Forzando https para cargar los estilos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;  //Para el deploy
 class AppServiceProvider extends ServiceProvider
 {
     /**
